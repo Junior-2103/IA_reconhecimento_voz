89b7fdf11fbf1ae14c3fa9f4593a54b435156301
José-w-Júnior <jjosewilker45@gmail.com>
2025-06-10 19:31:30 -0300

    Pegando os arquivos

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import whisper
 from pytubefix import YouTube
+import os
+
+if not os.path.exists('Videos'):
+    os.mkdir('Videos')
+
 yt = YouTube("https://www.youtube.com/watch?v=GCQl9RFns_0")
 stream = yt.streams.get_audio_only()
 stream.download('Videos')
 
 modelo = whisper.load_model("base")
 
-resposta = modelo.transcribe(f"Audio.m4a")
+arq = os.listdir('Videos')
+
+resposta = modelo.transcribe(f"Videos/{arq[0]}")
+
+os.remove(f'Videos/{arq[0]}')
 
 print(resposta["text"])
