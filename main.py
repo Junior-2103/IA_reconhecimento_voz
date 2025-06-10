745717aec0582ced1512e98e5f1724b1081435d5
José-w-Júnior <jjosewilker45@gmail.com>
2025-06-10 19:30:43 -0300

    Colocando o pytubefix

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import whisper
+from pytubefix import YouTube
+yt = YouTube("https://www.youtube.com/watch?v=GCQl9RFns_0")
+stream = yt.streams.get_audio_only()
+stream.download('Videos')
 
 modelo = whisper.load_model("base")
 
-resposta = modelo.transcribe(f"Audio.ogg")
+resposta = modelo.transcribe(f"Audio.m4a")
 
 print(resposta["text"])
