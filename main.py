a4a37c92769d95afe977e308d641ab3cdd9d5a6a
José-w-Júnior <jjosewilker45@gmail.com>
2025-06-10 21:50:43 -0300

    Adicionado IA para o texto

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import whisper
 from pytubefix import YouTube
+import google.generativeai as genai
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 if not os.path.exists('Videos'):
     os.mkdir('Videos')
 
-yt = YouTube("https://www.youtube.com/watch?v=GCQl9RFns_0")
+llm = ChatGoogleGenerativeAI(model='gemini-1.5-flash',temperature=0.8)
+
+yt = YouTube("https://www.youtube.com/watch?v=UdaHy0pxTl4")
 stream = yt.streams.get_audio_only()
 stream.download('Videos')
 
@@ -13,8 +20,21 @@ modelo = whisper.load_model("base")
 
 arq = os.listdir('Videos')
 
-resposta = modelo.transcribe(f"Videos/{arq[0]}")
+r = modelo.transcribe(f"Videos/{arq[0]}")
 
 os.remove(f'Videos/{arq[0]}')
 
-print(resposta["text"])
+messages = [
+    ('system',
+    '''
+    Você é uma IA que pega o roteiro de um video destaca os pontos principais do video, faz uma explicação do video,
+    falando sobre o tema, assunto, exemplos, etc.
+    E a resposta deve ser em MarkDown para a melhor visualização.
+    '''),
+    ('human',f'{r["text"]}'),
+]
+
+resposta = llm.invoke(messages)
+
+with open('a.md','w',encoding='utf-8') as arquivo:
+    arquivo.write(resposta.content)
