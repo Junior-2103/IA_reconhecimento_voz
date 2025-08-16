22f894e10e4b8132fa87a0acb13eeac2fd87ae8d
José-w-Júnior <jjosewilker45@gmail.com>
2025-08-16 20:10:56 -0300

    API Groq adicionado e melhoria de funções

--- main.py
+++ main.py
@@ -1,40 +1,123 @@
-import whisper
 from pytubefix import YouTube
-import google.generativeai as genai
-from langchain_google_genai import ChatGoogleGenerativeAI
-from dotenv import load_dotenv
+from groq import Groq
 import os
+from dotenv import load_dotenv
+import re
 
-load_dotenv()
+def create_folders(path_audio:str,path_summary_videos:str):
+    os.makedirs(path_audio,exist_ok=True)
+    os.makedirs(path_summary_videos,exist_ok=True)
 
-if not os.path.exists('Videos'):
-    os.mkdir('Videos')
+def download_youtube_audio(url:str,path_audio:str,filename:str='audio') -> str:
+    """
+    Baixa os áudios dos videos do Youtube com a URL informada.
+    Args:
+        url (str): Link do video do Youtube
+        path_audio (str): Local para baixar os arquivos de áudio
+        filename (str): Nome do arquivo de áudio
+    Returns:
+        str: Nome do arquivo de áudio
+    Raises:
+        Exception: Quaiquer erros
+        ValueError: Se nenhum áduio for encontrado
+    """
+    print('Baixando audio...')
+    try:
+        yt = YouTube(url)
+        stream = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
+        if not stream:
+            raise ValueError('Nenhum áudio do stream foi encontrado')
+        stream.download(path_audio,filename=f'{filename}.mp3')
+        return filename
+    except Exception as err:
+        raise Exception(f'Erro ao baixar video: {err}')
 
-llm = ChatGoogleGenerativeAI(model='gemini-1.5-flash',temperature=0.8)
+def transcribe_audio(path_audio:str, groq_client:Groq, model_transcription:str = "whisper-large-v3-turbo", language:str = 'pt') -> str:
+    """
+    Pega o áudio mp3 de `path_audio` e transcreve ele.
+    
+    Args:
+        path_audio (str): Local do arquivo de áudio
+        groq_client (Groq): Cliente do Groq
+        model_transcription (str): Nome do modelo de transcrição de áudio
+        language (str): Transcreve para (pt) 'Português'
+    Returns:
+        str: Transcrição do áudio
 
-yt = YouTube("https://www.youtube.com/watch?v=UdaHy0pxTl4")
-stream = yt.streams.get_audio_only()
-stream.download('Videos')
+    """
+    print('Transcrevendo audio...')
+    print(path_audio)
+    with open(path_audio,'rb') as file:
+        transcription = groq_client.audio.transcriptions.create(
+            file=(path_audio, file.read()),
+            model=model_transcription,
+            language=language,
+            response_format="verbose_json",
+        )
+    os.remove(path_audio)
+    return transcription.text
 
-modelo = whisper.load_model("base")
+def create_summary(transcription:str,groq_client:Groq,summary_filename:str,model_llm:str = 'qwen/qwen3-32b',language_markdown:str = 'Portugues-BR',out_dir:str = 'summary_videos'):
+    """
+    Cria um summario com a transcrição do áudio.
+    Args:
+        transcription (str): 
+        groq_client (Groq): 
+        summary_filename (str): 
+        model_llm (str): 
+        language_markdown (str): 
+        out_dir (str): 
+    Returns:
+        Arquivo na pasta `out_dir`
 
-arq = os.listdir('Videos')
+    """
+    summary_prompt = f"""
+        Você é uma IA que pega o roteiro de um video destaca os pontos principais do video, faz uma explicação do video,
+        falando sobre o tema, assunto, exemplos, etc.
+        E a resposta deve ser em **{language_markdown}**.
+        texto:
+        {transcription}
+    """
 
-r = modelo.transcribe(f"Videos/{arq[0]}")
+    print('Criando a melhor resposta...')
+    llm = groq_client.chat.completions.create(
+        model=model_llm,
+        messages=[
+            {
+            "role": "user",
+            "content": f"{summary_prompt}\n"
+            }
+        ],
+        temperature=0.6,
+        max_completion_tokens=4096,
+        top_p=0.95,
+        reasoning_effort="default",
+        stream=False,
+        stop=None
+    )
 
-os.remove(f'Videos/{arq[0]}')
+    summary_response = llm.choices[0].message.content
+    with open(f'{out_dir}/{summary_filename}.md','w',encoding='utf-8') as markdown:
+        text_markdown = re.sub(r'<think>.*?</think>','',summary_response, flags=re.DOTALL)
+        markdown.write(f'''{text_markdown}''')
+    print('Tudo Pronto!')
 
-messages = [
-    ('system',
-    '''
-    Você é uma IA que pega o roteiro de um video destaca os pontos principais do video, faz uma explicação do video,
-    falando sobre o tema, assunto, exemplos, etc.
-    E a resposta deve ser em MarkDown para a melhor visualização.
-    '''),
-    ('human',f'{r["text"]}'),
-]
+def main(url:str, markdown_filename:str, path_audio:str = 'videos_audios', path_summary_videos:str = 'summary_videos') -> None:
+    create_folders(path_audio,path_summary_videos)
 
-resposta = llm.invoke(messages)
+    loaded_env = load_dotenv()
+    if not loaded_env:
+        raise ValueError('Chave Api não encontrada. Defina GROQ_API_KEY no ambiente.')
+    
+    client = Groq()
 
-with open('a.md','w',encoding='utf-8') as arquivo:
-    arquivo.write(resposta.content)
+    audio_file_name = download_youtube_audio(url,path_audio)
+    audio_file = f'{path_audio}/{audio_file_name}.mp3'
+
+    transcription_data = transcribe_audio(audio_file,client)
+    create_summary(transcription_data,client,summary_filename=markdown_filename)
+
+if __name__ == '__main__':
+    url = input('URL do video: ')
+    summary_filename = input('Nome do Arquivo: ')
+    main(url,summary_filename)
